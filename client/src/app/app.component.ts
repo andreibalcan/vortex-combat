aad7d0c0098abc3dfd4d869d29994b19fb0730a6
andreibalcan <andreiflorinbalcan@gmail.com>
2025-03-22 16:50:22 +0000

    Improved server gitignore file

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { JsonPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
