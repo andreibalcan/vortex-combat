bf3f4537a72009827dcb93a1262715d05b2911d2
andreibalcan <andreiflorinbalcan@gmail.com>
2025-03-22 15:43:53 +0000

    Initial commit with Angular 19 client and .NET 9 server

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'client';
+
+  weatherData: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://localhost:5299/weatherforecast')
+      .subscribe(data => {
+        this.weatherData = data;
+      });
+  }
+}
